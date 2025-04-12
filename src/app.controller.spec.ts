498851d62e11fdb565885a459d31307a59106664
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-04-12 15:56:23 +0200

    feat: changed greeting

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Visit taskvault.hrustinszkiadam.tech to see the app!"', () => {
+      expect(appController.getRoot()).toBe(
+        'Visit taskvault.hrustinszkiadam.tech to see the app!',
+      );
     });
   });
 });
