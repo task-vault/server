cbd83649c628066222fc8c13addff186e91ab6c4
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-04-12 17:57:58 +0200

    feat: implement user management with database integration

--- src/drizzle/drizzle.service.ts
+++ src/drizzle/drizzle.service.ts
@@ -1,20 +1,34 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './drizzle.schema';
 
 @Injectable()
-export class DrizzleService implements OnModuleInit {
+export class DrizzleService implements OnModuleInit, OnModuleDestroy {
   private db: ReturnType<typeof drizzle> | null = null;
 
   constructor(private readonly configService: ConfigService) {}
 
-  onModuleInit() {
+  async onModuleInit() {
     const pool = new Pool({
-      connectionString: this.configService.get<string>('DATABASE_URL'),
+      user: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      database: this.configService.get<string>('DB_NAME'),
+      ssl: {
+        rejectUnauthorized: true,
+        ca: this.configService.get<string>('DB_CERT'),
+      },
     });
     this.db = drizzle(pool, { schema });
+
+    await this.db.$client.connect();
+  }
+
+  onModuleDestroy() {
+    this.db = null;
   }
 
   getDb() {
---
+++ src/users/interfaces/user.d.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id: string; // UUID
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+};
