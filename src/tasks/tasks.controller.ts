16411f4eb95a9bc9f735d034a739f65057d79502
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-04-18 14:27:15 +0200

    refactor: streamline task controller methods to utilize tasksService for task operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/require-await */
 import {
   Body,
   Controller,
@@ -13,7 +12,7 @@ import {
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { TaskGuard } from './guards/task-id.guard';
 import { TaskId } from './decorators/task-id.decorator';
-import { Task } from './interfaces/task';
+import { Task, TaskInsert, TaskResponse } from './interfaces/task';
 import { StateValidationPipe } from './pipes/state.pipe';
 import { State } from './interfaces/state-param';
 import { CreateTaskRequest } from './dto/create-task.request';
@@ -21,27 +20,28 @@ import { UpdateTaskRequest } from './dto/update-task.request';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { User } from '../users/interfaces/user';
 import { DeleteTasksRequest } from './dto/delete-tasks.request';
+import { TasksService } from './tasks.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('tasks')
 export class TasksController {
-  constructor() {}
+  constructor(private readonly tasksService: TasksService) {}
 
   @Get()
   async getAllTasks(@CurrentUser() user: User) {
-    return `All tasks retrieved successfully for user ${user.id}`;
+    return await this.tasksService.getAll(user.id);
   }
 
   @UseGuards(TaskGuard)
   @Get(':taskId')
   async getSingleTask(@TaskId() taskId: Task['id']) {
-    return `Task with ID ${taskId} retrieved successfully`;
+    return (await this.tasksService.getSingle(taskId)) as TaskResponse;
   }
 
   @UseGuards(TaskGuard)
   @Get(':taskId/progress')
   async getTaskProgress(@TaskId() taskId: Task['id']) {
-    return `Progress of task with ID ${taskId} retrieved successfully`;
+    return await this.tasksService.getProgress(taskId);
   }
 
   @Get('state/:state')
@@ -49,12 +49,17 @@ export class TasksController {
     @CurrentUser() user: User,
     @Param('state', StateValidationPipe) state: State,
   ) {
-    return `Tasks with state ${state} retrieved successfully for user ${user.id}`;
+    return await this.tasksService.getByState(user.id, state);
   }
 
   @Post()
   async createTask(@CurrentUser() user: User, @Body() task: CreateTaskRequest) {
-    return `Task with title ${task.title} created successfully for user ${user.id}`;
+    const taskData: TaskInsert = {
+      ...task,
+      userId: user.id,
+    };
+
+    return await this.tasksService.create(taskData);
   }
 
   @UseGuards(TaskGuard)
@@ -64,28 +69,37 @@ export class TasksController {
     @TaskId() taskId: Task['id'],
     @Body() task: UpdateTaskRequest,
   ) {
-    return `Task with ID ${taskId} updated successfully ${task.title} for user ${user.id}`;
+    const taskData: TaskInsert = {
+      ...task,
+      userId: user.id,
+    };
+
+    return await this.tasksService.update(taskId, taskData);
   }
 
   @UseGuards(TaskGuard)
   @HttpCode(204)
   @Delete(':taskId')
   async deleteTask(@TaskId() taskId: Task['id']) {
-    return `Task with ID ${taskId} deleted successfully`;
+    return await this.tasksService.delete(taskId);
   }
 
   @UseGuards(TaskGuard)
   @HttpCode(200)
   @Post(':taskId/complete')
   async completeTask(@TaskId() taskId: Task['id']) {
-    return `Task with ID ${taskId} marked as completed successfully`;
+    return await this.tasksService.update(taskId, {
+      completed: true,
+    });
   }
 
   @UseGuards(TaskGuard)
   @HttpCode(200)
   @Post(':taskId/uncomplete')
   async uncompleteTask(@TaskId() taskId: Task['id']) {
-    return `Task with ID ${taskId} marked as uncompleted successfully`;
+    return await this.tasksService.update(taskId, {
+      completed: false,
+    });
   }
 
   @HttpCode(204)
@@ -94,6 +108,6 @@ export class TasksController {
     @CurrentUser() user: User,
     @Body() body: DeleteTasksRequest,
   ) {
-    return `Tasks with IDs [${body.taskIds.join(', ')}] deleted successfully for user ${user.id}`;
+    return await this.tasksService.deleteMany(user.id, body.taskIds);
   }
 }
